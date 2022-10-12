1e60f3068d8ebfe0bfa28b441f2095ba13947c3c
zini06 <zinnia.sarkhel@gmail.com>
2022-10-11 19:37:39 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(function () {
+  $(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+      $(".hamburger").addClass("whenscrolled");
+      $("nav").addClass("stickyadd");
+      $("nav").removeClass("stick");
+    } else {
+      $(".hamburger").removeClass("whenscrolled");
+      $("nav").removeClass("stickyadd");
+      $("nav").addClass("stick");
+    }
+  });
+
+  $('.navbar-nav a[href^="#"]').on("click", function (event) {
+    event.preventDefault();
+    $("html,body").animate(
+      {
+        scrollTop: $(this.hash).offset().top,
+      },
+      1000
+    );
+  });
+});
